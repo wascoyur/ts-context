7e0375eb7ec9d9dec118c748f200ddc7a90a48a4
yuriy.vasilev <#Mywasco1>
2022-04-29 14:43:22 +0500

    https://blog.logrocket.com/how-to-use-react-context-typescript/

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,7 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import ThemedButton from './context/themed-button';
 
 function App() {
-  return (
-    <div className='App'>
-      <ThemedButton />
-    </div>
-  );
+  return <div className='App'>App</div>;
 }
 
 export default App;
