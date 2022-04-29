d66c9948c23a224a9735176250e2c5d8817c0ff3
yuriy.vasilev <#Mywasco1>
2022-04-29 15:38:14 +0500

    refactoring

--- src/context/TodoContext.tsx
+++ src/context/TodoContext.tsx
@@ -1,4 +1,38 @@
-import React from 'react';
-import { TodoContextType } from '../types/types';
+import React, { FC, useState } from 'react';
+import { ITodo, TodoContextType } from '../types/types';
 
 export const TodoContext = React.createContext<TodoContextType | null>(null);
+export const TodoProvider: FC<React.ReactNode> = () => {
+  const [todos, setTodos] = useState<ITodo[]>([
+    {
+      id: 1,
+      title: 'post 1',
+      description: 'this is a description',
+      status: false,
+    },
+    {
+      id: 2,
+      title: 'post 2',
+      description: 'this is a description',
+      status: true,
+    },
+  ]);
+  const saveTodo = (todo: ITodo) => {
+    const newTodo: ITodo = {
+      id: Math.random(),
+      title: todo.title,
+      description: todo.description,
+      status: todo.status,
+    };
+    setTodos([...todos, newTodo]);
+  };
+  const updateTodo = (id: number) => {
+    todos.filter((todo: ITodo) => {
+      if (todo.id === id) {
+        todo.status = true;
+        setTodos([...todos]);
+      }
+    });
+  };
+  return null;
+};
--- src/context/TodoProvider.tsx
+++ src/context/TodoProvider.tsx
@@ -1,38 +1,3 @@
 import React, { FC, useState } from 'react';
 import { toEditorSettings } from 'typescript';
 import { ITodo } from '../types/types';
-
-export const TodoProvider: FC<React.ReactNode> = () => {
-  const [todos, setTodos] = useState<ITodo[]>([
-    {
-      id: 1,
-      title: 'post 1',
-      description: 'this is a description',
-      status: false,
-    },
-    {
-      id: 2,
-      title: 'post 2',
-      description: 'this is a description',
-      status: true,
-    },
-  ]);
-  const saveTodo = (todo: ITodo) => {
-    const newTodo: ITodo = {
-      id: Math.random(),
-      title: todo.title,
-      description: todo.description,
-      status: todo.status,
-    };
-    setTodos([...todos, newTodo]);
-  };
-  const updateTodo = (id: number) => {
-    todos.filter((todo: ITodo) => {
-      if (todo.id === id) {
-        todo.status = true;
-        setTodos([...todos]);
-      }
-    });
-  };
-  return null;
-};
