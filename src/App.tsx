0219f097e7e9e827c659b9d1c8765f2634fc3dfa
yuriy.vasilev <#Mywasco1>
2022-04-29 17:45:43 +0500

    add fetch

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,25 @@ import './App.css';
 import { PostsList } from './component/PostsList';
 import { postsContextDefaultValue } from './context/postsContextDefaultValue';
 import { PostsContext } from './context/PostsContext';
+import { useCallback, useState } from 'react';
+import { Post } from './context/interfaces';
 
 function App() {
+   const [posts, setPosts] = useState<Post[]>([]);
+   const [isLoading, setIsLoading] = useState(false);
+
+   const fetchPosts = useCallback(() => {
+     setIsLoading(true);
+     fetch('https://jsonplaceholder.typicode.com/posts')
+       .then((response) => response.json())
+       .then((fetchedPosts) => {
+         setPosts(fetchedPosts);
+       })
+       .finally(() => {
+         setIsLoading(false);
+       });
+   }, [setPosts]);
+
   return (
     <PostsContext.Provider value={postsContextDefaultValue}>
       <div className='App'>App</div>
